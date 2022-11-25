fbcc1fba3ce3a9179e5829f291a3324e2463054c
lauradevuono <lauradevuono@gmail.com>
2022-11-25 15:36:33 -0500

    added hamburger menu! other small changes..

---
+++ app.js
@@ -0,0 +1,31 @@
+// hamburger menu tutorial here: https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
+
+// hamburger menu
+// create variables for each elemeent
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+// function to toggle hamburger menu on click
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+// hide the menu when the user clicks on a nav item
+menuItems.forEach(
+    function (menuItem) {
+        menuItem.addEventListener("click", toggleMenu);
+    }
+)
