a10082660b2cfc41ad6e537d2f6b580dc33949c3
lauradevuono <lauradevuono@gmail.com>
2022-11-26 15:34:30 -0500

    playing around with dark mode settings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // hamburger menu tutorial here: https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
 
 // hamburger menu
-// create variables for each elemeent
+// create variables for each element
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
@@ -29,3 +29,23 @@ menuItems.forEach(
         menuItem.addEventListener("click", toggleMenu);
     }
 )
+
+
+// dark mode
+const body = document.querySelector("body");
+const lightSwitch = document.querySelector(".lightSwitch")
+
+const wolf = document.querySelector(".wolf");
+const cat = document.querySelector(".cat");
+const imageBox = document.querySelector(".introImg")
+console.log(imageBox)
+
+lightSwitch.addEventListener("click", (e) => {
+    body.classList.toggle("darkMode");
+})
+
+// if (body.classList.contains("darkMode")) {
+//     imageBox.appendChild(".cat")
+// } else {
+//     imageBox.remove(".cat");
+// }
